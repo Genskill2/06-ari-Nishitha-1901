1e0ebc4880830ada40ebcc835e02d9e5b95a48d4
Nishitha-1901 <66087134+Nishitha-1901@users.noreply.github.com>
2021-06-25 19:28:18 +0300

    Update ari.c

--- ari.c
+++ ari.c
@@ -1,15 +1,15 @@
 #include<stdio.h>
-#include<cs50>
+#include<cs50,h>
 #include<ctype.h>
 #include<string.h>
 #include<math.h>
 
-stdin *ari(stdin *s)
+string ari(string s)
 {
     int noc = 0,now = 0; nos = 0;
     for (int i=0; s[i]!='\0' ; i++)
     {
-        if (isalnum())
+        if (isalnum(s[i]))
         {
         noc++;
         }
@@ -17,69 +17,69 @@ stdin *ari(stdin *s)
         {
         now++;
         }
-        else if (s[i]=='.')
+        else if (s[i]=='.' || s[i]=='?' || s[i]=='!')
         {
         nos++;
         }
     }
     
         float a=0;
-        a= (4.71*(noc/now)+(0.5*(now/nos))-21.43;
-        a=round(a);
+        a= (4.71*((float)noc/(float)now)+(0.5*((float)now/(float)nos))-21.43;
+        int r_a=a+1;
           
-        if (a==1)
+        if (r_a==1)
             {
                 return ("Kindergarten");
             }
-        else if (a==2)
+        else if (r_a==2)
             {
                 return ("First/Second Grade");
             }
-        else if (a==3)
+        else if (r_a==3)
             {
                 return ("Third Grade");
             }
-        else if (a==4)
+        else if (r_a==4)
             {
                 return ("Fourth Grade");
             }
-        else if (a==5)
+        else if (r_a==5)
             {
                 return ("Fifth Grade");
             }
-        else if (a==6)
+        else if (r_a==6)
             {
                 return ("Sixth Grade");
             }
-        else if (a==7)
+        else if (r_a==7)
             {
                 return ("Seventh Grade");
             }
-        else if (a==8)
+        else if (r_a==8)
             {
                 return ("Eighth Grade");
             }
-        else if (a==9)
+        else if (r_a==9)
             {
                 return ("Ninth Grade");
             }
-        else if (a==10)
+        else if (r_a==10)
             {
                 return ("Tenth Grade");
             }
-        else if (a==11)
+        else if (r_a==11)
             {
                 return ("Eleventh Grade");
             }
-        else if (a==12)
+        else if (r_a==12)
             {
                 return ("Twelfth Grade");
             }
-        else if (a==13)
+        else if (r_a==13)
             {
                 return ("College Student");
             }
-        else if (a==14)
+        else if (r_a==14)
             {
                 return ("Professor");
             }
