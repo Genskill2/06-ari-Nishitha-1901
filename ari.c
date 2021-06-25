abc23d81f0f68469ec8ac0ee5adee40c8c255594
Nishitha-1901 <66087134+Nishitha-1901@users.noreply.github.com>
2021-06-25 19:10:35 +0300

    Update ari.c

--- ari.c
+++ ari.c
@@ -3,7 +3,7 @@
 #include<string.h>
 #include<math.h>
 
-char ari(char s)
+string ari(string s)
 {
     int noc = 0,now = 0; nos = 0;
     for (int i=0; s[i]!='\0' ; i++)
@@ -22,10 +22,64 @@ char ari(char s)
         }
     }
     
-    float ARI=0;
-    ARI= (4.71*(noc/now)+(0.5*(now/nos))-21.43;
-    ARI=round(ARI);
-    
-    
-    
+        float a=0;
+        a= (4.71*(noc/now)+(0.5*(now/nos))-21.43;
+        a=round(a);
+          
+        if (a==1)
+            {
+                return ("Kindergarten");
+            }
+        else if (a==2)
+            {
+                return ("First/Second Grade");
+            }
+        else if (a==3)
+            {
+                return ("Third Grade");
+            }
+        else if (a==4)
+            {
+                return ("Fourth Grade");
+            }
+        else if (a==5)
+            {
+                return ("Fifth Grade");
+            }
+        else if (a==6)
+            {
+                return ("Sixth Grade");
+            }
+        else if (a==7)
+            {
+                return ("Seventh Grade");
+            }
+        else if (a==8)
+            {
+                return ("Eighth Grade");
+            }
+        else if (a==9)
+            {
+                return ("Ninth Grade");
+            }
+        else if (a==10)
+            {
+                return ("Tenth Grade");
+            }
+        else if (a==11)
+            {
+                return ("Eleventh Grade");
+            }
+        else if (a==12)
+            {
+                return ("Twelfth Grade");
+            }
+        else if (a==13)
+            {
+                return ("College Student");
+            }
+        else if (a==14)
+            {
+                return ("Professor");
+            }
 }
